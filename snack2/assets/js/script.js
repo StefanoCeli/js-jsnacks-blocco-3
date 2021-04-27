bbb35c4f00a693a78175c0c6f5f1fdbaf236eeff
StefanoCeli <stefanopsn91@gmail.com>
2021-04-27 12:55:57 +0200

    fine terzo snack

--- snack2/assets/js/script.js
+++ snack2/assets/js/script.js
@@ -1,56 +1,58 @@
 $(document).ready(function(){
-var zucchine = [
-    {
-        varietà : 'zucchino nero di Milano',
-        peso : 1.5,
-        lunghezza : 30
-    },
-    {
-        varietà : 'zucchino romanesco',
-        peso : 1.2,
-        lunghezza : 23
-    },
-    {
-        varietà : 'zucchino ortolano di Faenza',
-        peso : 1,
-        lunghezza : 15
-    },
-    {
-        varietà : 'zucchina lunga fiorentina',
-        peso : 0.8,
-        lunghezza : 14
-    },
-    {
-        varietà : 'zucchino siciliano',
-        peso : 1.3,
-        lunghezza : 27
-    },
-    {
-        varietà : 'zucchina Striata di Napoli',
-        peso : 1.4,
-        lunghezza : 17
-    },
-    {
-        varietà : 'zucchina bianca triestina',
-        peso : 1.8,
-        lunghezza : 28
-    },
-    {
-        varietà : 'zucchina rigata Pugliese',
-        peso : 1.2,
-        lunghezza : 13
-    },
-    {
-        varietà : 'zucchino tondo di piacenza',
-        peso : 1.7,
-        lunghezza : 12
-    },
-    {
-        varietà : 'zucchino tondo di Nizza',
-        peso : 2,
-        lunghezza : 13 
-    }
+    
+    var zucchine = [
+        {
+            varietà : 'zucchino nero di Milano',
+            peso : 1.5,
+            lunghezza : 30
+        },
+        {
+            varietà : 'zucchino romanesco',
+            peso : 1.2,
+            lunghezza : 23
+        },
+        {
+            varietà : 'zucchino ortolano di Faenza',
+            peso : 1,
+            lunghezza : 15
+        },
+        {
+            varietà : 'zucchina lunga fiorentina',
+            peso : 0.8,
+            lunghezza : 14
+        },
+        {
+            varietà : 'zucchino siciliano',
+            peso : 1.3,
+            lunghezza : 27
+        },
+        {
+            varietà : 'zucchina Striata di Napoli',
+            peso : 1.4,
+            lunghezza : 17
+        },
+        {
+            varietà : 'zucchina bianca triestina',
+            peso : 1.8,
+            lunghezza : 28
+        },
+        {
+            varietà : 'zucchina rigata Pugliese',
+            peso : 1.2,
+            lunghezza : 13
+        },
+        {
+            varietà : 'zucchino tondo di piacenza',
+            peso : 1.7,
+            lunghezza : 12
+        },
+        {
+            varietà : 'zucchino tondo di Nizza',
+            peso : 2,
+            lunghezza : 13 
+        }
 ]
+
 var zucchinePiccole = [];
 var zucchineLunghe = [];
 var pesoPiccole= 0;
@@ -65,6 +67,7 @@ for(var zucchina of zucchine){
         pesoLunghe += zucchina['peso']
     }
 }
+
 console.log('zucchinePiccole:');
 console.log(zucchinePiccole);
 console.log('il peso totale delle zucchine corte è: ' + pesoPiccole);
--- snack3/assets/js/script.js
+++ snack3/assets/js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+    var arrNum = [1,2,3,4,5];
+    var arrLettere = ['a','b','c','d','e','f','g'];
+    var arrFuso = [];
 
+    fondiArr();
+    
+    function fondiArr(){
+        if(arrNum.length < arrLettere.length){
+            var i = 0;
+            while(i < arrNum.length){
+                arrFuso.push(arrNum[i]);
+                arrFuso.push(arrLettere[i]);
+                i++
+            }
+        }else if(arrLettere.length < arrNum.length){
+            var i = 0;
+            while(i < arrLettere.length){
+                arrFuso.push(arrNum[i]);
+                arrFuso.push(arrLettere[i]);
+                i++
+            }
+        }
+    }
+    console.log(arrFuso);
 });
