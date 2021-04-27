18cc77ef1f1175b584ff37f5636cba1411b31900
StefanoCeli <stefanopsn91@gmail.com>
2021-04-27 12:15:20 +0200

    fine secondo snack

---
+++ snack2/assets/js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+var zucchine = [
+    {
+        varietà : 'zucchino nero di Milano',
+        peso : 1.5,
+        lunghezza : 30
+    },
+    {
+        varietà : 'zucchino romanesco',
+        peso : 1.2,
+        lunghezza : 23
+    },
+    {
+        varietà : 'zucchino ortolano di Faenza',
+        peso : 1,
+        lunghezza : 15
+    },
+    {
+        varietà : 'zucchina lunga fiorentina',
+        peso : 0.8,
+        lunghezza : 14
+    },
+    {
+        varietà : 'zucchino siciliano',
+        peso : 1.3,
+        lunghezza : 27
+    },
+    {
+        varietà : 'zucchina Striata di Napoli',
+        peso : 1.4,
+        lunghezza : 17
+    },
+    {
+        varietà : 'zucchina bianca triestina',
+        peso : 1.8,
+        lunghezza : 28
+    },
+    {
+        varietà : 'zucchina rigata Pugliese',
+        peso : 1.2,
+        lunghezza : 13
+    },
+    {
+        varietà : 'zucchino tondo di piacenza',
+        peso : 1.7,
+        lunghezza : 12
+    },
+    {
+        varietà : 'zucchino tondo di Nizza',
+        peso : 2,
+        lunghezza : 13 
+    }
+]
+var zucchinePiccole = [];
+var zucchineLunghe = [];
+
+for(var zucchina of zucchine){
+    if(zucchina['lunghezza'] <= 15 ){
+        zucchinePiccole.push(zucchina);
+    }else{
+        zucchineLunghe.push(zucchina);
+    }
+}
+console.log('zucchinePiccole:');
+console.log(zucchinePiccole);
+console.log('zucchineLunghe:');
+console.log(zucchineLunghe);
+
+});
